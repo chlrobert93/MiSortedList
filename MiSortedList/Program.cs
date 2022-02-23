9042a5b94dc719b0d588a0cd015abafcd00b0840
Eder <chlrobert93@gmail.com>
2022-02-22 20:13:55 -0600

    Add project files.

---
+++ MiSortedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+
+namespace MiSortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList lista = new SortedList();
+            
+            lista.Add(4, "Cuarto");
+            lista.Add(2, "Segundo");
+            lista.Add(3, "Tercero");
+            lista.Add(1, "Primero");
+
+            foreach (object clave in lista.Keys)
+            {
+                Console.WriteLine("{0} - {1}", clave.ToString(), lista[clave].ToString());
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
